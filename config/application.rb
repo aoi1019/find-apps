b9a243a519a088d50c2531eadfd5df184df79791
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-06 01:23:45 +0900

    rails g controller staticpages

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module FindApps
     config.generators do |g|
       g.assets false
       g.test_framework false
-
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
