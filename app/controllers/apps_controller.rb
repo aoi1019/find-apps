3a7d24f0ed94396489933d5cc0cc1c432bec697a
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-06 22:08:22 +0900

    app create

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,7 +1,22 @@
 class AppsController < ApplicationController
   before_action :logged_in_user
-  
+
   def new
     @app = App.new
   end
+
+  def create
+    @app = current_user.apps.build(app_params)
+    if @app.save
+      flash[:success] = "アプリの投稿が完了しました！"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def app_params
+      params.require(:app).permit(:name, :description, :point, :period, :reference)
+    end
 end
