b72896b7de77db19acca1c84b049047973124a2d
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-06 18:56:11 +0900

    g model migration

---
+++ db/migrate/20201106095524_create_apps.rb
@@ -0,0 +1,14 @@
+class CreateApps < ActiveRecord::Migration[5.2]
+  def change
+    create_table :apps do |t|
+      t.string :name
+      t.text :description
+      t.text :point
+      t.text :reference
+      t.integer :period
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+    add_index :apps, [:user_id, :created_at]
+  end
+end
---
+++ spec/factories/apps.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :app do
+    name "MyString"
+    description "MyText"
+    point "MyText"
+    reference "MyText"
+    period 1
+    user nil
+  end
+end
