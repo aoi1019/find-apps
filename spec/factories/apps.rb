1f5d28e4c8d9302e8bc19fef56a293715d8cdc1c
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-06 21:39:57 +0900

    destroy test

--- spec/factories/apps.rb
+++ spec/factories/apps.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :app do
-    name {"アプリ"}
-    description {"インスタグラムです"}
-    point {"Ruby on Railsで開発！"}
-    reference {"https://find-apps.herokuapp.com/"}
-    period {10}
+    name { "アプリ" }
+    description { "インスタグラムです" }
+    point { "Ruby on Railsで開発！" }
+    reference { "https://find-apps.herokuapp.com/" }
+    period { 10 }
     association :user
   end
 end
--- spec/requests/users_destroy_spec.rb
+++ spec/requests/users_destroy_spec.rb
@@ -1,15 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "ユーザーの削除", type: :request do
-  let!(:admin_user) { create(:user, :admin) }
-  let!(:user) { create(:user) }
-  let!(:other_user) { create(:user) }
+  before do
+    @admin_user = FactoryBot.create(:user, :admin)
+    @user = FactoryBot.create(:user)
+    @other_user = FactoryBot.create(:user)
+    @app = FactoryBot.create(:app, user: @user)
+  end
 
   context "管理者ユーザーの場合" do
     it "ユーザーを削除後、ユーザー一覧ページにリダイレクト" do
-      login_for_request(admin_user)
+      login_for_request(@admin_user)
       expect {
-        delete user_path(user)
+        delete user_path(@user)
       }.to change(User, :count).by(-1)
       redirect_to users_url
       follow_redirect!
@@ -19,17 +22,17 @@ RSpec.describe "ユーザーの削除", type: :request do
 
   context "管理者以外のユーザーの場合" do
     it "自分のアカウントを削除できること" do
-      login_for_request(user)
+      login_for_request(@user)
       expect {
-        delete user_path(user)
+        delete user_path(@user)
       }.to change(User, :count).by(-1)
       redirect_to root_url
     end
 
     it "自分以外のユーザーを削除しようとすると、トップページへリダイレクトすること" do
-      login_for_request(user)
+      login_for_request(@user)
       expect {
-        delete user_path(other_user)
+        delete user_path(@other_user)
       }.not_to change(User, :count)
       expect(response).to have_http_status "302"
       expect(response).to redirect_to root_path
@@ -39,10 +42,19 @@ RSpec.describe "ユーザーの削除", type: :request do
   context "ログインしていないユーザーの場合" do
     it "ログインページへリダイレクトすること" do
       expect {
-        delete user_path(user)
+        delete user_path(@user)
       }.not_to change(User, :count)
       expect(response).to have_http_status "302"
       expect(response).to redirect_to login_path
+    end
+  end
+
+  context "アプリが紐づくユーザーを削除した場合" do
+    it "ユーザーと同時に紐づくアプリも削除される" do
+      login_for_request(@user)
+      expect {
+        delete user_path(@user)
+      }.to change(App, :count).by(-1)
     end
   end
 end
