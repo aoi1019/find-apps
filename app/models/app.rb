99b79e481e623986f8d9beb25576a16c98b16b1e
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-06 18:59:22 +0900

    edit model

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,7 @@
 class App < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description, length: { maximum: 140 }
+  validates :point, length: { maximum: 50 }
 end
