763be094832b366b4c72270e5d8d5214bfd2ef07
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-06 17:07:11 +0900

    friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,6 +25,15 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
