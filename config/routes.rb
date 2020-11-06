1edfc1cbc3687111f344672cb312b721e2114225
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-06 11:16:12 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
-  get 'users/index'
   root 'static_pages#home'
+  get :signup,       to: 'users#new'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
+  resources :users
 end
