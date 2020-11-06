ed618d6229e3205b7f81a10af0d11d709bff2676
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-06 19:03:12 +0900

    model test

--- spec/factories/apps.rb
+++ spec/factories/apps.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :app do
-    name "MyString"
-    description "MyText"
-    point "MyText"
-    reference "MyText"
-    period 1
-    user nil
+    name {"アプリ"}
+    description {"インスタグラムです"}
+    point {"Ruby on Railsで開発！"}
+    reference {"https://find-apps.herokuapp.com/"}
+    period {10}
+    association :user
   end
 end
