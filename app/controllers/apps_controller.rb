44b087aaaec29025534b7937ed90442cebe4a6e4
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-06 23:41:49 +0900

    destroy setting

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -34,6 +34,18 @@ class AppsController < ApplicationController
     end
   end
 
+  def destroy
+    @app = App.find(params[:id])
+    if current_user.admin? || current_user?(@app.user)
+      @app.destroy
+      flash[:success] = "アプリが削除されました"
+      redirect_to request.referrer == user_url(@app.user) ? user_url(@app.user) : root_path
+    else
+      flash[:danger] = "他人のアプリは削除できません"
+      redirect_to root_path
+    end
+  end
+
   private
     def app_params
       params.require(:app).permit(:name, :description, :point, :period, :reference)
