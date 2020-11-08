bca5edd1456293e6a213d88a9ecfc263870c830f
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-08 13:35:35 +0900

    create destroy method

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,10 +2,21 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def create
-    
+    @app = App.find(params[:app_id])
+    @user = @app.user
+    current_user.favorite(@app)
+    respond_to do |format|
+      format.html {redirect_to request.referrer || root_path}
+      format.js
+    end
   end
 
   def destroy
-    
+    @app = App.find(params[:app_id])
+    current_user.favorites.find_by(app_id: @app.id).destroy
+    respond_to do |format|
+      format.html {redirect_to request.referrer || root_path}
+      format.js
+    end
   end
 end
