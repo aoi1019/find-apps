c1b50332cecfcffa5b223f235a059ab0aad5bb55
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-09 00:00:03 +0900

    edit model

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,6 +1,7 @@
 class App < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 30 }
