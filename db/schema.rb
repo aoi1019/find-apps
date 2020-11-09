a71c7329e622dafcc88347c9d6e11b702a1ad8f8
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-10 00:40:23 +0900

    edit migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_024251) do
+ActiveRecord::Schema.define(version: 2020_11_09_153916) do
 
   create_table "apps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 2020_11_09_024251) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "app_id"], name: "index_favorites_on_user_id_and_app_id", unique: true
+  end
+
+  create_table "logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "app_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["app_id"], name: "index_logs_on_app_id"
   end
 
   create_table "notifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
---
+++ spec/factories/logs.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :log do
+    app_id 1
+    content "MyText"
+  end
+end
